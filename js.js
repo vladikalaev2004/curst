6c9a2a53f70460329e40296d2640b27b9d253a61
Alan <vladikalaev15@gmail.com>
2020-11-12 13:31:17 +0500

    Написали вторую часть приложения

--- js.js
+++ js.js
@@ -1,4 +1,42 @@
-const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?');
+// ЦИКЛЫ
+
+let num = 50;
+
+// while ( num <= 55 ) {
+//     console.log( num );
+//     num++;
+// }
+
+// do {
+//     console.log( num );
+//     num++;
+// }while( num < 55 );
+
+
+// for (let i = 0; i < 10 ; i++ ) {
+//     if( i === 6 ) {
+//         continue;
+//     } 
+//     console.log(i)
+// }
+// УСЛОВИЕ
+// const num = 50;
+
+// switch( num ) {
+//     case 49:
+//     console.log('Неверно');
+//     break;
+//     case 100:
+//     console.log('Неверно');
+//     break;
+//     case 51:
+//     console.log('В самую точку')
+//     break;  
+//     } 
+
+
+// СОЗДАЁМ ПРИЛОЖЕНИЕ
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?');
 
 const personalMovie = {
     count:numberOfFilms,
@@ -7,15 +45,59 @@ const personalMovie = {
     genres:[],
     privat:false,
 }
+let i = 0;
 
-const a = prompt('Какой последний фильм вы посмотрели?',''),
-      b = prompt('Как бы ва его оценили?',''),
-      c = prompt('Какой последний фильм вы посмотрели?',''),
-      d = prompt('Как бы ва его оценили?','');
+while( i < 2 ) {
+    const a = prompt('Какой последний фильм вы посмотрели?','');
+          b = prompt('Как бы ва его оценили?','');
+          if( a != null && b != null && a != '' && b != '' && a.length < 50 ) {
+            personalMovie.movies[a] = b;
+            console.log('Done');
+            i++
+          }else {
+            console.log("Error");
+            i--;
+          }
+}
 
-personalMovie.movies[a] = b;
-personalMovie.movies[c] = d;
 
-console.log( personalMovie.count );
-console.log( personalMovie.movies.logan );
-console.log( personalMovie.movies.lookAt );  
+// do {
+//     const a = prompt('Какой последний фильм вы посмотрели?','');
+//           b = prompt('Как бы ва его оценили?','');
+//           if( a != null && b != null && a != '' && b != '' && a.length < 50 ) {
+//             personalMovie.movies[a] = b;
+//             console.log('Done');
+//             i++
+//           }else {
+//             console.log("Error");
+//             i--;
+//           }
+// }while( i < 2 )
+
+
+// for( let i = 0; i < 2; i++ ) {
+//     const a = prompt('Какой последний фильм вы посмотрели?','');
+//           b = prompt('Как бы ва его оценили?','');
+//           if( a != null && b != null && a != '' && b != '' && a.length < 50 ) {
+//             personalMovie.movies[a] = b;
+//             console.log('Done');
+//           }else {
+//             console.log("Error");
+//             i--;
+//           }
+          
+// }
+
+
+if( personalMovie.count < 10 ) {
+    alert('Просмотрено довольно мало фильмов')
+}else if( personalMovie.count >= 10 && personalMovie.count <= 30 ) {
+    alert('Вы классический зритель');
+}else if( personalMovie.count > 30 ) {
+    console.log('Вы киноман');
+}
+else {
+    alert('Произошла ошибка');
+}
+
+console.log( personalMovie );
